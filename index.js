593f77b5a0b8e2b3f496ddbfe701b716a7a3f650
samlak <samlak1999@gmail.com>
2020-03-12 17:31:38 +0100

    switching to heroku for deployment

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-'use strict';
+var express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const axios = require("axios");
+const TelegramBot = require('node-telegram-bot-api');
 
-const Telegram = require('telegram-node-bot');
-const tg = new Telegram.Telegram('1103650145:AAEW7Q3b3raimpT5Ftm892K3jYGtn9rM0zE', {
-        workers: 1
+const bot = new TelegramBot(process.env.TELEGRAM_API_TOKEN, {polling: true});
+
+let openWeatherUrl = process.env.OPENWEATHER_API_URL;
+
+
+var app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.post("/start", function(req, res) {
+    bot.onText(/\/check/, (msg) => {
+        bot.sendMessage(msg.chat.id, "Checking the weather information in *" + msg.text.split(' ')[1] + "*", {parse_mode : "Markdown"});
+        let location = msg.text.split(' ')[1];
+        getForecast(location).then((result) => {
+            bot.sendMessage(msg.chat.id, result);
+        });
     });
 
-const PingController = require('./controllers/ping');
-const OtherwiseController = require('./controllers/otherwise');
 
-tg.router.when(new Telegram.TextCommand('/ping', 'pingCommand'), new PingController())
-    .otherwise(new OtherwiseController());
+    function getForecast(location){
+        let newUrl = openWeatherUrl + location+"&appid="+process.env.OPENWEATHER_API_KEY;
+        return axios.get(newUrl).then(response => {
+            let temp = response.data.main.temp;
+            //converts temperature from kelvin to celsuis
+            temp = Math.round(temp - 273.15); 
+            let locationName = response.data.name;
+            let res = "It's " + temp + " degrees in " + locationName;
+            return res;
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+});
+
+app.listen(3000, () => console.log("Telegram bot is listening on port 3000!"));
 
